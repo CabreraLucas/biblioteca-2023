e4c16f8b8ca8537e472d8154be560d81186fed47
CabreraLucas <100152927+CabreraLucas@users.noreply.github.com>
2023-11-06 22:19:18 +0000

    Livro

--- app/src/main/java/application/controller/LivroController.java
+++ app/src/main/java/application/controller/LivroController.java
@@ -3,20 +3,29 @@ package application.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import orgn.springframework.ui.Model;
+import org.springframework.ui.Model;
 
+import application.model.GeneroRepository;
 import application.model.LivroRepository;
 
 
 @Controller
 @RequestMapping("/Livro")
 public class LivroController {
-
     @Autowired 
     private LivroRepository livroRepo;
+    @Autowired
+    private GeneroRepository generoRepo;
     
+    @RequestMapping("/list")
     public String list(Model model){
-        model.addAtribute("livros", livroRepo.findAll());
+        model.addAttribute("livros", livroRepo.findAll());
         return "livro/list";
+    }
+
+    @RequestMapping("/insert")
+    public String insert(){
+
+        return "livro/insert";
     }
 }
